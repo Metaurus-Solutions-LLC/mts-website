f75b5451f6bfc1ea46ec40e0a47a1a9f8ef5fa94
Ted Wetekamp <ted.wetekamp@yahoo.com>
2024-10-29 17:06:49 -0500

    Update WhoWeAre page with leadership team draft.

--- src/components/WhoWeAre.jsx
+++ src/components/WhoWeAre.jsx
@@ -33,18 +33,26 @@ function WhoWeAre() {
                <div className="mx-2 my-20 bg-black"></div>
              </div>
              <h2  className="flex mx-16 my-4 text-amber-300">Leadership Team</h2>
-             <p  className="mt-8 mx-16 text-xs my-4 md:text-xl">The battle was historically significant and a great victory for the Roman Empire.  The Romans had lost previous battles against the Carthaginians and were faced with significant challenges prior to the Battle of the Metaurus.  Claudius Nero's leadership was key at several points leading up to, during and following the battle.</p>
-             <p  className="mt-8 mx-16 text-xs my-4 md:text-xl">The key elements contributing to the success for the Romans were:</p>
-               <ul className="mx-16 text-xs md:text-xl">
-                <li><strong>Strategy - </strong>Claudius Nero repositioned his forces to create the conditions for defeating Hasdrubal while simultaneously holding Hannibal's forces in place to prevent the Carthiginian armies from joining into a single force.</li>
-                <li><strong>Teamwork - </strong>The three roman consuls joined their forces at the Metaurus River and worked together cohesively to attack and defeat Hasdrubal.</li>
-                <li><strong>Coordination - </strong>The three roman consuls coordinated their efforts before and during the battle.  This allowed Claudius Nero's forces to arrive at night undetected until the next morning.</li>
-                <li><strong>Execution - </strong>Claudius Nero maintained awareness of the situation throughout the battle.  He planned and executed and adjusted to the situation with skill.</li>
-                <li><strong>Effort - </strong>The Roman force commanded by Claudius Nero marched 250 miles in 7 days.  This drive and focus of effort along with maintaining the appearance that this force was still located in the South was equivalant to the Roman Army being in two places at once.</li>
-                <li><strong>Tactics - </strong>The tactical maneuver from one flank of the Roman attack to the other flipped the obstacle into an advantage.  The ravine was also a barrier to the Carthaginians and protected the Roman right wing.  Once the Roman rotation was executed the ravine prevented the Carthaginians from also attacking from their left and along with the Metaurus River, prevented them from escaping the Roman attack.</li>
-                <li><strong>Resilience - </strong>The Roman army pressed and pushed with maximum effort.  The leaders problem-solved and adjusted to the situation.  The entire army worked and fought together to defeat the Carthaginians.</li>
-                <li><strong>Courage - </strong>From the time that Hannibal's army first arrived in Italy the outcome was never certain and at times a Roman victory was even in doubt.  Yet the Roman consuls lead their armies and generated sense of urgency coupled with belief in ultimate success and victory over the Carthaginians.</li>
-               </ul>
+             <p  className="mt-8 mx-16 text-xs my-4 md:text-xl">At Metaurus Solutions, people are at the heart of everything we do. We foster an inclusive culture that values diversity, collaboration, and personal growth. Our team members are encouraged to bring their unique perspectives to the table, creating a dynamic environment where innovation thrives. We invest in continuous learning and development, ensuring that everyone has the tools and opportunities to excel. By prioritizing well-being and work-life balance, we create a supportive atmosphere where our people can thrive, knowing their contributions are recognized and valued.</p>
+             <p  className="mt-8 mx-16 text-xs my-4 md:text-xl">Ted Wetekamp</p>
+               <div className="mx-20 my-8 text-xs md:text-xl">
+                <p className="my-4">Ted is an experienced manager for the delivery of technical and IT transformation projects including software applications, cloud hosting, data center expansion and telecommunications upgrade projects. I facilitate customer and partner strategic projects and programs. Ted leads combined teams to engineer, build and deploy resilient cloud products and solutions. </p>
+                <p className="my-4">Ted is a certified Project Management Professional (PMP®) with over 20 years of project and program management experience across diverse markets and industries. Ted excels at planning, implementing and leveraging emerging technologies to achieve innovative and cost-effective product and service solutions. Ted is an effective team builder who is skilled at direction of remote and local project teams. Ted has a proven track record at developing and managing successful startup operations.</p>
+                <p className="my-4">In addition, Ted is pursuing multiple cloud certifications.</p>
+                <p className="my-4">As part of the MultiCloud Specialization, Ted implemented Cloud projects based on real-world scenarios such as:</p>
+                <p> - Migrating users into an AWS enterprise environment.</p>
+                <p>Learn more about these cloud projects by visiting Ted's Cloud Portfolio at: https://medium.com/@ted.wetekamp</p>
+                <p>Technologies of interest:</p>
+                <p>Cloud | MultiCloud | DevOps | AWS | Microsoft Azure | Google Cloud | Oracle Cloud | Terraform | Ansible</p>
+               </div>
+             <p  className="mt-8 mx-16 text-xs my-4 md:text-xl">Femi Ajibade</p>
+               <div className="mx-20 my-8 text-xs md:text-xl">
+                <p className="my-4">Femi's role as DevOps Technical Lead intertwines AWS excellence with Chef.io configuration mastery, streamlining cloud services for optimal performance. Femi's academic roots in Earth Science from the University of Ilorin, coupled with advanced computer science credits from Dallas College, have cemented his technical foundation. </p>
+                <p className="my-4">As an Observability Engineer, Femi cultivates a resilient infrastructure, ensuring high availability and robust monitoring capabilities, which speaks to his commitment to continuous improvement and the pursuit of fault-tolerant systems. Our team's advancements in observability reflect our dedication to operational excellence and the delivery of dependable cloud solutions.</p>
+                <p>Learn more about these cloud projects by visiting Femi's Cloud Portfolio at: https://medium.com/@femi.Ajibade</p>
+                <p>Technologies of interest:</p>
+                <p>Cloud | MultiCloud | DevOps | AWS | Microsoft Azure | Google Cloud | Oracle Cloud | Terraform | Ansible</p>
+               </div>
            </div>
            {/* topic-quote future section */}
            <div> {/* topic-question */}
